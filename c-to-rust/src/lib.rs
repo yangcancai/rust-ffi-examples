4fe5eaf0254b65bbbc7cf5fd4c5beff32c2f6314
admin <admin.com>
2021-11-12 11:44:03 +0800

    Add: Create rust struct then C language to execute it and free resource..

--- c-to-rust/src/lib.rs
+++ c-to-rust/src/lib.rs
@@ -1,6 +1,61 @@
 #![crate_type = "staticlib"]
+use std::ffi::CString;
+use std::os::raw::c_char;
+#[repr(C)]
+pub struct Player {
+    name: * const c_char,
+}
+impl Default for Player {
+   fn default() -> Self {
+       Player{
+           name: CString::new("admin").unwrap().into_raw()
+       }
+   } 
+}
+impl Player {
+    fn new(name: *const c_char) -> Self {
+        // let name = CString::new(name).unwrap().as_ptr();
+        // unsafe {println!("new player ...{:?}", CStr::from_ptr(name).to_str().unwrap());}
+        Player { name }
+    }
+}
+impl Drop for Player{
+    fn drop(&mut self) {
+    //   let name =unsafe {CStr::from_ptr(self.name).to_str().unwrap_or_default()};
+    //    println!("drop player...");
+    }
+}
+#[no_mangle]
+pub extern "C" fn double_input(input: i32) -> i32 {
+    input * 2
+}
+#[no_mangle]
+pub extern "C" fn create_player(name: * const c_char) -> Player {
+    Player::new(name)
+}
+#[no_mangle]
+pub extern "C" fn default_player() -> Player {
+    Player::default()
+}
+#[no_mangle]
+pub extern "C" fn create_player_pointer(name: * const c_char) -> *mut Player{
+    let p = Box::new(Player::new(name));
+    Box::into_raw(p)
+}
 
 #[no_mangle]
-pub extern fn double_input(input: i32) -> i32 {
-    input * 2
+pub extern "C" fn free_player(p: Player){
+    drop(p);
+}
+#[no_mangle]
+pub unsafe extern "C" fn free_player_pointer(p: *mut Player){
+    let _p = Box::from_raw(p);
+}
+#[no_mangle]
+pub extern "C" fn check_char() -> * const c_char{
+    CString::new("check..").unwrap().into_raw()
+}
+#[no_mangle]
+pub unsafe extern "C" fn free_char(c: *const c_char){
+   CString::from_raw(c as *mut c_char);
 }
--- c-to-rust/src/main.c
+++ c-to-rust/src/main.c
@@ -1,11 +1,43 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct Player
+{
+    char* name;
+}Player;
 
 extern int32_t double_input(int32_t input);
-
+extern Player create_player(char* name);
+extern Player* create_player_pointer(char* name);
+extern void free_player_pointer(Player* p);
+extern void free_player(Player p);
+extern char* check_char();
+extern void free_char();
+extern Player default_player();
 int main() {
     int input = 4;
+    Player p;
+    Player* p1;
+    Player def;
     int output = double_input(input);
     printf("%d * 2 = %d\n", input, output);
+    p = create_player("hello");
+    printf("player name = %s\n", p.name);
+    p1 = create_player_pointer("hello1");
+    printf("player name = %s\n", p1->name);
+    free_player(p);
+    // free_player_pointer(p1);
+    // or
+    free(p1);
+    char* c = check_char();
+    printf("c=%s",c);
+    // free_char(c);
+    // or
+    free(c);
+    def= default_player();
+    printf("player name = %s\n", def.name);
+    free_char(def.name);
+    // or
+    // free(def.name);
     return 0;
 }
